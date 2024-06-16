e3f77cf02b30077b78f1188834636a0e17127fe7
Nimi77 <abimbolaoladejo4@gmail.com>
2024-06-16 10:08:26 +0100

    product layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,131 @@
-import { useState } from 'react'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState,useEffect } from "react";
+import "./App.css";
+import axios from 'axios'
+import ProductList from "./Components/ProductList";
+import Cart from "./Components/Cart";
 
 function App() {
- 
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [currentProductIndex, setCurrentProductIndex] = useState(0);
+  const [quantities, setQuantities] = useState({});
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const response = await axios.get("https://dummyjson.com/products");
+        const data = response.data;
+        setProducts(data.products)
+
+        // Initialize quantities state as an object with product IDs as keys and 0 as the initial quantity
+        const initialQuantities = data.products.reduce((acc, product) => {
+          acc[product.id] = 0;
+          return acc;
+        }, {});
+        setQuantities(initialQuantities);
+        
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+    fetchProducts();
+  }, []);
+
+  const addToCart = (product) => {
+    alert("product a been added to cart");
+    // Ensure product has all required properties
+    if (!product.id || !product.title || !product.price) {
+      console.error("Product is missing required properties", product);
+      return;
+    }
+    //updating the [cart] state
+    setCart((prevCart) => {
+      const existingProduct = prevCart.find((item) => item.id === product.id); // Checks if the product is already in the cart
+
+      // Increase item quantities
+      if (existingProduct) {
+        return prevCart.map((item) =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + quantities[product.id] }
+            : item
+        );
+      } else {
+        return [...prevCart, { ...product, quantity: quantities[product.id] }]; // If the product is not in the cart, add it with quantity 1
+      }
+    });
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [product.id]: 0,
+    })); // Reset quantity after adding to cart
+  };
+
+  const removeFromCart = (productId) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id !== productId));
+  };
+
+  const reduceQuantity = (productId) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: Math.max(prevQuantities[productId] - 1, 0),
+    }));
+  };
+
+  const increaseQuantity = (productId) => {
+    setQuantities((prevQuantities) => ({
+      ...prevQuantities,
+      [productId]: prevQuantities[productId] + 1,
+    }));
+  };
+
+  //pagnation button
+  const handleNext = () => {
+    setCurrentProductIndex((prevIndex) =>
+      Math.min(prevIndex + 1, products.length - 3)
+    );
+  };
+  const handlePrev = () => {
+    setCurrentProductIndex((prevIndex) => Math.max(0, prevIndex - 1));
+  };
+
+  const displayedProducts = products.slice(
+    currentProductIndex,
+    currentProductIndex + 3
+  );
+
+  return (
+    <div className="container">
+      <div className="layout">
+        {products.length > 0 && (
+          <ProductList
+            products={displayedProducts}
+            reduceQuantity={reduceQuantity}
+            increaseQuantity={increaseQuantity}
+            quantities={quantities}
+            addToCart={addToCart}
+          />
+        )}
+        <div className="controls">
+          <button onClick={handlePrev} disabled={currentProductIndex === 0}>
+            Prev
+          </button>
+          <button
+            onClick={handleNext}
+            disabled={currentProductIndex >= products.length - 3}
+          >
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+{
+  /* <h1>Product List</h1>
+    
+      <h2>Cart</h2>
+      <Cart cart={cart} removeFromCart={removeFromCart} reduceQuantity={reduceQuantity}></Cart> */
+}
---
+++ src/Components/ProductList.jsx
@@ -0,0 +1,69 @@
+import PropTypes from "prop-types";
+
+const capitalizeFirstLetter = (string) => {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+const truncateDescription = (description) => {
+  const words = description.split(" ");
+  if (words.length > 14) {
+    return `${words
+      .slice(0, 14)
+      .join(" ")}... <a href="#" class="read-more-link">Read more</a>`;
+  }
+  return description;
+};
+
+const ProductList = ({ products,reduceQuantity, increaseQuantity, addToCart, quantities }) => {
+  return (
+    <div id="products" className="product-lists">
+      <div className="heading">
+        <h2>Available Products</h2>
+      </div>
+
+      <div className="product-container">
+        {products.map((product) => (
+          <div key={product.id} className="product">
+            <div className="img-container">
+              <img src={product.images} alt="product-image" />
+            </div>
+            <div className="product-details">
+              <div className="p-heading">
+                <span>{capitalizeFirstLetter(product.category)}</span>
+                <h2>{product.title}</h2>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: truncateDescription(product.description),
+                  }}
+                ></p>
+              </div>
+              <div className="add-remove">
+                <button onClick={() => reduceQuantity(product.id)}> - </button>
+                <span>{quantities[product.id]}</span>
+                <button onClick={() => increaseQuantity(product.id)}> + </button>
+              </div>
+              <button onClick={() => addToCart(product)} className="addCart">
+                Add to Cart
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+ProductList.propTypes = {
+  products: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+    })
+  ).isRequired,
+  addToCart: PropTypes.func.isRequired,
+  reduceQuantity: PropTypes.func.isRequired,
+  increaseQuantity: PropTypes.func.isRequired,
+  quantities: PropTypes.objectOf(PropTypes.number).isRequired,
+};
+
+export default ProductList;
